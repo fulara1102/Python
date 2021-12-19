2f0c08d55b53fd8a8b68b734b710f9c281b86a90
Gaurav Fulara <77954499+fulara1102@users.noreply.github.com>
2021-12-19 22:01:38 +0530

    Update HR 3.py

--- HR 3.py
+++ HR 3.py
@@ -2,5 +2,5 @@ if __name__ == '__main__':
     a = int(input())
     b = int(input())
     c = a//b #for float division    
-    d = a/b  #for integetr division
+    d = a/b  #for integer division
     print("%d\n%f\n" % (c,d))
