ac84aadc8064135f244108946cbd420702b5f5ba
Gaurav Fulara <77954499+fulara1102@users.noreply.github.com>
2021-12-21 20:11:07 +0530

    Update Sum+sum.py

--- Sum+sum.py
+++ Sum+sum.py
@@ -1,8 +1,9 @@
-n = int(input())
+#Sum of n natural number
+n = int(input())        #input from user
 sum = 0
 i =1
-while(i <= n):
+while(i <= n):         #while loop
     sum +=i
-    print(i)
+    print(i)           
     i = i +1
 print("Sum is",sum )
