424cce9684db206848be9c5a492755b8b324e8d3
Gaurav Fulara <77954499+fulara1102@users.noreply.github.com>
2021-12-26 20:28:41 +0530

    Add files via upload

---
+++ String slicing.py
@@ -0,0 +1,8 @@
+mystr = "I feel proud to be a hindu"
+print(len(mystr))
+print(type(mystr))
+print(mystr[::1])
+print(mystr.capitalize())
+print(mystr.replace("hindu","bhagwa"))
+print(mystr.count("e"))
+print(mystr.upper())
